b856537e54dea7370a349fcaa994a28f6f318c60
tomorrow anan <tomorrowanan@gmail.com>
2016-04-29 13:10:38 +0900

    Yahooトップページのタイトル取得に成功

---
+++ trafficIntoScrapy/trafficIntoScrapy/spiders/TargetSplider.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from trafficIntoScrapy.items import TrafficInfoItem
+
+class TargetSplider(CrawlSpider):
+        # scrapyをCLIから実行するときの識別子
+        name = 'hello'
+        # spiderに探査を許可するドメイン
+        allowed_domains = ["www.yahoo.co.jp"]
+        # 起点(探査を開始する)URL
+        start_urls = ["http://www.yahoo.co.jp/"]
+        # LinkExtractorの引数で特定のルール(例えばURLにnewを含むページのみスクレイプするなど)を指定可能だが、今回は全てのページを対象とするため引数はなし
+        # Ruleにマッチしたページをダウンロードすると、callbackに指定した関数が呼ばれる
+        # followをTrueにすると、再帰的に探査を行う
+        rules = [Rule(LinkExtractor(), callback='parse_pageinfo', follow=True)]
+        
+        def parse_pageinfo(self, response):
+                sel = Selector(response)
+                item = TrafficInfoItem()
+                item['URL'] = response.url
+                # ページのどの部分をスクレイプするかを指定
+                # xPath形式での指定に加え、CSS形式での指定も可能
+                item['title'] = sel.xpath('/html/head/title/text()').extract()
+                return item
+        
+        
